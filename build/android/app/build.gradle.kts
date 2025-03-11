1bdd807a6d5e364d5e8cf97a3969710edbcb1234
Taku Yamamoto <jts0ymt@gmail.com>
2025-03-11 21:53:24 +0900

    Add C++ Module to Android Project

--- build/android/app/build.gradle.kts
+++ build/android/app/build.gradle.kts
@@ -15,6 +15,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        externalNativeBuild {
+            cmake {
+                cppFlags += ""
+            }
+        }
     }
 
     buildTypes {
@@ -32,6 +37,12 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("../../../luna/CMakeLists.txt")
+            version = "3.31.6"
+        }
     }
 }
 
